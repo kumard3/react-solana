90c7a45d9c5f64b2b3fbcb722926538daf2d1ddb
kumard3 <kumardeepanshu157200@gmail.com>
2022-02-19 22:36:49 +0530

    Connect to Wallet button added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import twitterLogo from "./assets/twitter-logo.svg";
+import React, { useEffect } from 'react';
+import './App.css';
+import twitterLogo from './assets/twitter-logo.svg';
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  // Actions
-
-  /*
-   * Declare your function
-   */
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
 
-      if (solana && solana.isPhantom) {
-        console.log("Phantom wallet found!");
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+          );
+        }
       } else {
-        alert("Solana object not found! Get a Phantom Wallet 👻");
+        alert('Solana object not found! Get a Phantom Wallet 👻');
       }
     } catch (error) {
       console.error(error);
@@ -27,15 +29,30 @@ const App = () => {
   };
 
   /*
-   * When our component first mounts, let's check to see if we have a connected
-   * Phantom Wallet
+   * Let's define this method so our code doesn't break.
+   * We will write the logic for this next!
    */
+  const connectWallet = async () => {};
+
+  /*
+   * We want to render this UI when the user hasn't connected
+   * their wallet to our app yet.
+   */
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
   useEffect(() => {
     const onLoad = async () => {
       await checkIfWalletIsConnected();
     };
-    window.addEventListener("load", onLoad);
-    return () => window.removeEventListener("load", onLoad);
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
   }, []);
 
   return (
@@ -44,6 +61,8 @@ const App = () => {
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {/* Render your connect to wallet button right here */}
+          {renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
